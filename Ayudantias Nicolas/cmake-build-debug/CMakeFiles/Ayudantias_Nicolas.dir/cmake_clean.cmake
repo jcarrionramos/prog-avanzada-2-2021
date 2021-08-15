b25e092eca632e250630778f879f1ebfd5870f7f
NicoMonc <nicolas.moncada1@mail.udp.cl>
2021-08-14 21:56:31 -0400

    Creacion carpeta y primeras pruebas

---
+++ Ayudantias Nicolas/cmake-build-debug/CMakeFiles/Ayudantias_Nicolas.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "Ayudantias_Nicolas.exe"
+  "Ayudantias_Nicolas.exe.manifest"
+  "Ayudantias_Nicolas.pdb"
+  "CMakeFiles/Ayudantias_Nicolas.dir/main.cpp.obj"
+  "libAyudantias_Nicolas.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Ayudantias_Nicolas.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
