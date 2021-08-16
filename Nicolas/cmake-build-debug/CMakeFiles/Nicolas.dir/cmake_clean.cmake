37086b1a8faff2c9c63f4fd3dbc41c0bf9baad49
NicoMonc <nicolas.moncada1@mail.udp.cl>
2021-08-15 23:56:02 -0400

    Create

---
+++ Nicolas/cmake-build-debug/CMakeFiles/Nicolas.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Nicolas.dir/main.cpp.obj"
+  "Nicolas.exe"
+  "Nicolas.exe.manifest"
+  "Nicolas.pdb"
+  "libNicolas.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Nicolas.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
