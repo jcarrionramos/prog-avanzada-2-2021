a08dea2b4f9c959e038a1d4c3674bef6f70ab8b1
jcarrionramos <javiercarrionramos@gmail.com>
2021-10-19 14:28:43 -0300

    stack

--- estructuras/stack.cpp
+++ estructuras/stack.cpp
@@ -1,19 +1,35 @@
 #include "iostream"
 #include "stack"
+#include "vector"
 
 using namespace std;
 
-int main() {
-  stack<int> s;
+class Pair {
+  public:
+  int first;
+  int second;
 
-  s.push(1);
-  s.push(2);
-  s.push(3);
-  s.push(4);
-  s.push(5);
+  Pair(int first, int second) : first(first), second(second) {
+  }
+};
+
+class PairChild : public Pair {
+  public:
+  PairChild(int first, int second) : Pair(first, second) {
+  }
+};
+
+// LIFO = Last In First Out
+
+int main() {
+  stack<Pair> s;
+  
+  s.push(Pair(2,3));
+  s.push(PairChild(1,1));
 
   while(!s.empty()) {
-    cout << s.top() << ". El tamanio es es: " << s.size() << endl;
+    Pair current = s.top();
+    cout << current.first << "," << current.second << ". El tamanio es es: " << s.size() << endl;
     s.pop();
   }
 
